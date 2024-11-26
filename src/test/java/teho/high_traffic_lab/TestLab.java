9b1d0cfb16151026c8f0d455ad7d2954165641a6
teho <xogh8755@naver.com>
2024-11-26 15:59:26 +0900

    saveAll() VS bulk insert

--- src/test/java/teho/high_traffic_lab/TestLab.java
+++ src/test/java/teho/high_traffic_lab/TestLab.java
@@ -18,21 +18,32 @@ public class TestLab {
     @Autowired
     UserService userService;
 
+//    @Test
+//    @DisplayName("단순 조회 시간 확인")
+//    public void findAllUsersTimeTest() {
+//        long startTime = System.currentTimeMillis();
+//        List<User> all = userRepository.findAll();
+//        long endTime = System.currentTimeMillis();
+//        System.out.println("조회 시간(ms): " + (endTime - startTime));
+//        Assertions.assertThat(all).isNotNull();
+//    }
+
     @Test
-    @DisplayName("단순 조회 시간 확인")
-    public void findAllUsersTimeTest() {
+    @DisplayName("user level 조건 1개 조회 시간 확인")
+    public void findAllUsersByLevelTimeTest() {
         long startTime = System.currentTimeMillis();
-        List<User> all = userRepository.findAll();
+        List<User> all = userRepository.findAllByLevel(3);
         long endTime = System.currentTimeMillis();
         System.out.println("조회 시간(ms): " + (endTime - startTime));
+        System.out.println("조회된 객체 수:" + all.size());
         Assertions.assertThat(all).isNotNull();
     }
 
     @Test
-    @DisplayName("where 조건 1개 조회 시간 확인")
-    public void findAllUsersByLevelTimeTest() {
+    @DisplayName("user age 조건 1개 조회 시간 확인")
+    public void findAllUsersByAgeTimeTest() {
         long startTime = System.currentTimeMillis();
-        List<User> all = userRepository.findAllByLevel(200);
+        List<User> all = userRepository.findAllByAge(20);
         long endTime = System.currentTimeMillis();
         System.out.println("조회 시간(ms): " + (endTime - startTime));
         System.out.println("조회된 객체 수:" + all.size());
