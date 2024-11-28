471a3eb5d5c2c1b49f912443e1a917b7077a8354
teho <xogh8755@naver.com>
2024-11-28 17:02:07 +0900

    redis atomic 연산, kafak 활용한 Coupon 선착순 발급

--- high_traffic_consumer1/src/main/java/teho/high_traffic_lab/repository/CouponRepository.java
+++
@@ -1,7 +0,0 @@
-package teho.high_traffic_lab.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import teho.high_traffic_lab.entity.Coupon;
-
-public interface CouponRepository extends JpaRepository<Coupon, Long> {
-}
